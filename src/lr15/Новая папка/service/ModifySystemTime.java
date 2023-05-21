eda92b9d588f55d54856dcce6a6897eb75cba38a
GlassySky99 <maksim_zudinov@bk.ru>
2023-05-21 14:25:17 +0500

    1

---
+++ src/lr15/Новая папка/service/ModifySystemTime.java
@@ -0,0 +1,18 @@
+package com.example.test_rest_service1.service;
+
+import com.example.test_rest_service1.model.Response;
+import com.example.test_rest_service1.model.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Qualifier("ModifySystemTime")
+public class ModifySystemTime implements MyModifyService{
+    @Override
+    public Response modify(Response response){
+        response.setSystemTime("");
+        return response;
+    }
+}
